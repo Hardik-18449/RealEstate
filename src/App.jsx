0f112f24ed463b139d14a877a5399e908499ba06
Mohit Kumawat <mohitk.bheemainfotech@gmail.com>
2026-02-18 23:26:07 +0530

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import './App.css'
 import Products from './components/Products'
-import DataFetcher from './components/DataFetcher'
 
 function App() {
   return (
    <>
    <Products/>
-   <DataFetcher/>
+   
    </>
   )
 }
